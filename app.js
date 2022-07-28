6600f5f88ed1df45ff9fee0021e7c79d5fe85fe9
Tanner Richards <Tanner.richards990@gmail.com>
2022-07-28 13:11:33 -0700

    added html code for webpage layout, described my const

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 // import functions and grab DOM elements
+const cityDropdown = document.getElementById('city-dropdown');
+const seasonDropdown = document.getElementById('season-dropdown');
+const terrainDropdown = document.getElementById('terrain-dropdown');
+const cityEl = document.getElementById('city');
+const seasonEl = document.getElementById('season');
+const terrainEl = document.getElementById('terrain');
+const reportEl = document.getElementById('report');
+const slogansEl = document.getElementById('slogans');
+const sloganInput = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
 
 // let state
 
